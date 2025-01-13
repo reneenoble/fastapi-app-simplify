97577e36453f58c6f8733586c6d09cce51a58269
reneenoble <renee.noble@hotmail.com>
2025-01-13 17:24:41 +1100

    Cut out non-core looking items

--- src/fastapi_app/app.py
+++ src/fastapi_app/app.py
@@ -1,18 +1,14 @@
-import os
+import sys, os
 import pathlib
 from typing import Annotated
+import uvicorn
 
-from azure.monitor.opentelemetry import configure_azure_monitor
-from fastapi import FastAPI, Form, Request
-from fastapi.responses import HTMLResponse
+from fastapi import FastAPI, Request
+from fastapi.responses import PlainTextResponse
+
 from fastapi.staticfiles import StaticFiles
 from fastapi.templating import Jinja2Templates
-from sqlmodel import Session, select
 
-from .models import Cruise, Destination, InfoRequest, engine
-
-if os.getenv("APPLICATIONINSIGHTS_CONNECTION_STRING"):
-    configure_azure_monitor()
 
 app = FastAPI()
 parent_path = pathlib.Path(__file__).parent.parent
@@ -23,60 +19,11 @@ templates.env.globals["prod"] = os.environ.get("RUNNING_IN_PRODUCTION", False)
 templates.env.globals["url_for"] = app.url_path_for
 
 
-@app.get("/", response_class=HTMLResponse)
+@app.get("/", response_class=PlainTextResponse)
 def index(request: Request):
-    return templates.TemplateResponse("index.html", {"request": request})
+    version = sys.version_info
+    return PlainTextResponse(content=f"Hello World, I am Python {version.major}.{version.minor}")
 
-
-@app.get("/about", response_class=HTMLResponse)
-def about(request: Request):
-    return templates.TemplateResponse("about.html", {"request": request})
-
-
-@app.get("/destinations", response_class=HTMLResponse)
-def destinations(request: Request):
-    with Session(engine) as session:
-        all_destinations = session.exec(select(Destination)).all()
-    return templates.TemplateResponse("destinations.html", {"request": request, "destinations": all_destinations})
-
-
-@app.get("/destination/{pk}", response_class=HTMLResponse)
-def destination_detail(request: Request, pk: int):
-    with Session(engine) as session:
-        destination = session.exec(select(Destination).where(Destination.id == pk)).first()
-        return templates.TemplateResponse(
-            "destination_detail.html", {"request": request, "destination": destination, "cruises": destination.cruises}
-        )
-
-
-@app.get("/cruise/{pk}")
-def cruise_detail(request: Request, pk: int):
-    with Session(engine) as session:
-        cruise = session.exec(select(Cruise).where(Cruise.id == pk)).first()
-        return templates.TemplateResponse(
-            "cruise_detail.html", {"request": request, "cruise": cruise, "destinations": cruise.destinations}
-        )
-
-
-@app.get("/info_request/", response_class=HTMLResponse)
-def info_request(request: Request):
-    with Session(engine) as session:
-        all_cruises = session.exec(select(Cruise)).all()
-        return templates.TemplateResponse("info_request_create.html", {"request": request, "cruises": all_cruises})
-
-
-@app.post("/info_request/", response_model=InfoRequest)
-def create_info_request(request: Request, info_request: Annotated[InfoRequest, Form()]):
-    with Session(engine) as session:
-        session.add(info_request)
-        session.commit()
-        session.refresh(info_request)
-        all_cruises = session.exec(select(Cruise)).all()
-        return templates.TemplateResponse(
-            "info_request_create.html",
-            {
-                "request": request,
-                "cruises": all_cruises,
-                "message": "Information request submitted.",
-            },
-        )
+if __name__ == '__main__':
+    import uvicorn
+    uvicorn.run(app, host="0.0.0.0", port=8000)
